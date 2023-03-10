294163cc8f246dd4065d276b81a6a0fe58a7703a
eduardonk9999 <thiago.novak7@gmail.com>
2023-03-10 13:36:56 -0300

    teste pinMode

---
+++ traffic-lights.ino
@@ -0,0 +1,16 @@
+// name of ports =>
+int red = 10;
+//int yellow = 9;
+//int green = 8;
+
+void setup() {
+  //quais portas vamos usar no arduino...
+  pinMode(red, OUTPUT);
+  //pinMode(yellow, OUTPUT);
+  //pinMode(green, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red, HIGH);
+
+}
